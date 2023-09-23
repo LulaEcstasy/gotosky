8a026578ce8ee0a635592c69259d88e6c730e681
LulaEcstasy <42184824+LulaEcstasy@users.noreply.github.com>
2023-09-23 18:18:14 +0200

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="author" content="Kamil Gotowski">
+<meta name="title" content="MUZYCZKY">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<link rel="stylesheet" href="stylephone.css"> 
+<link rel="stylesheet" href="styletale.css">
+
+
+
+<style>
+
+body { 
+  font-family: Arial, Helvetica, sans-serif;
+    background: url('/img/tlo.jpg') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    height: auto;
+width: auto;
+margin: 0;
+}
+
+a:link {
+  color: pink;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: none;
+}
+#header {
+    background-color: rgba(0, 0, 0, 0.0);
+  padding: 0px 0px;
+  color: #ff3300;
+  text-align: center;
+  font-size: 30px; 
+  font-weight: bold;
+  position: fixed;
+  top: 0;
+  width: 60%;
+}
+
+
+
+table, th, td {
+    border: 10px double rgba(0, 0, 0, 0.2);
+	    border-color: #E9A8EA;
+		border-width: 10px;
+		    background-color: rgba(0, 0, 0, 0.4);
+  padding: 5px;
+text-align: center;
+position:center;
+margin: auto;
+padding-left: 10px;
+padding-right: 10px;
+padding-bottom: 10px;
+padding-top: 10px;
+  width: auto;
+}
+
+
+ img {
+     margin: auto;
+	 position: center;
+width: 85%;
+height: 85%;
+
+}
+
+    .crop {
+        width: 480px;
+        height: 640px;
+    }
+        .crop img {
+        width: 480px;
+        height: 640px;
+    }
+	td image 
+{
+    display: block;
+	margin: auto;
+	position: center;
+	
+}
+
+.transbox000 {				
+    border: 10px double rgba(0, 0, 0, 0.2);
+	    border-color: #E9A8EA;
+		    background-color: rgba(0, 0, 0, 0.2);
+  padding: 5px;
+text-align: center;
+position:center;
+margin: auto;
+padding-left: 10px;
+padding-right: 10px;
+padding-bottom: 10px;
+padding-top: 10px;
+}
+h1, h2, h3 {
+color: pink;
+margin-top: 10px;
+    margin-bottom: 5px;
+}
+
+
+table {
+  min-width: 75%;
+}
+
+form {
+  min-width: 0;
+}
+
+</style>
+
+
+</head>
+
+<body>
+
+<table>
+
+<tr><td><br>
+<h2><b> Wszystko do sciagniecia na </b><h2>
+<h3><a href="https://mega.nz/folder/TkkhBILK#5y3AZmk1E019a629Uk7C0w">mega.nz</a></h3>
+<br></td></tr>
+  <tr><td>      <b><font size="5" color="pink">          	Children Of The Night                                                                                        </font></b>    <p>                <img src="https://img.youtube.com/vi/u6zrXboCSaI/hqdefault.jpg" width="480" height="320">                 <p> 
+<div><iframe class="transparent1"
+   frameborder="0"
+   width="300"
+   height="100"
+allowtransparency="true"
+background-image="url('https://img.youtube.com/vi/qvmC4cpLG1s/hqdefault.jpg')"
+   src="https://drive.google.com/file/d/1RmSznjdSWV4Lk4MLNyIShBVnYBgx9p8E/preview">
+</iframe></div>
+<iframe 
+   frameborder="0"
+   width="300"
+   height="100"
+allowtransparency="true"
+background-image="url('https://img.youtube.com/vi/qvmC4cpLG1s/hqdefault.jpg')"
+   src="https://mega.nz/file/Ct0gkQAa#Ms3Tu10gvoAFsuE5vnKFM7MK9psuStW7c6nid3YY-aY">
+</iframe>
+
+<audio controls>
+  <source src="https://drive.google.com/file/d/1RmSznjdSWV4Lk4MLNyIShBVnYBgx9p8E/preview">
+</audio>
+  <p><a href="https://www.youtube.com/watch?v=u6zrXboCSaI">https://www.youtube.com/watch?v=u6zrXboCSaI</a></p></td></tr>
+  <tr><td>      <b><font size="5" color="pink">          	Santogold - Youll Find a Way                                                        </font></b>    <p>                <img src="https://img.youtube.com/vi/VnAQL-VL3w4/hqdefault.jpg" width="480" height="320">                           <p><a href="https://www.youtube.com/watch?v=VnAQL-VL3w4">https://www.youtube.com/watch?v=VnAQL-VL3w4</a></p></td></tr>
+  <tr><td>      <b><font size="5" color="pink">          	The Road Im On                                                                               </font></b>    <p>                <img src="https://img.youtube.com/vi/qvmC4cpLG1s/hqdefault.jpg" width="480" height="320">                           <p><a href="https://www.youtube.com/watch?v=qvmC4cpLG1s">https://www.youtube.com/watch?v=qvmC4cpLG1s</a></p></td></tr>
+  <tr><td>      <b><font size="5" color="pink">          	BLR - Comfort Me                                                        </font></b>    <p>                <img src="https://img.youtube.com/vi/tFU2nhcu2Fs/hqdefault.jpg" width="480" height="320">                           <p><a href="https://www.youtube.com/watch?v=tFU2nhcu2Fs">https://www.youtube.com/watch?v=tFU2nhcu2Fs</a></p></td></tr>
+  <tr><td>      <b><font size="5" color="pink">          	Pixies - Where is my Mine                                                                    </font></b>    <p>                <img src="https://img.youtube.com/vi/7QXDkrkdmc8/hqdefault.jpg" width="480" height="320">                           <p><a href="https://www.youtube.com/watch?v=7QXDkrkdmc8">https://www.youtube.com/watch?v=7QXDkrkdmc8</a></p>
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/7QXDkrkdmc8?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></td></tr>
+</table>
+
+
+	</body>
+</html>	
